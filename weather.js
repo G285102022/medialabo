240b89509b0e7d23c7385033a4d054dc1ba0f41d
G285102022 <228510@st.takushoku-u.ac.jp>
2023-07-20 16:21:19 +0900

    コミット

--- weather.js
+++ weather.js
@@ -98,6 +98,17 @@ ul.insertAdjacentElement('beforeend', li8);
 let b = document.querySelector('button#btn');
 b.addEventListener('click', showSelectResult);
 
+
+li = document.createElement('li');
+li2 = document.createElement('li');
+li3 = document.createElement('li');
+li4 = document.createElement('li');
+li5 = document.createElement('li');
+li6 = document.createElement('li');
+li7 = document.createElement('li');
+li8 = document.createElement('li');
+
+
 function showSelectResult() {
     let s = document.querySelector('select#timei');
     let idx = s.selectedIndex;  // idx 番目の option が選択された
@@ -108,5 +119,55 @@ function showSelectResult() {
     console.log('選択された ' + idx + ' 番目の option の情報:');
     console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
     console.log('  textContent='+o.textContent);
+
+    a = o.getAttribute('value');
+    url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+a+'.json';
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
 }
+
+    // 通信が成功した時の処理
+    function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    div = document.querySelector('div#result');
+    ul = document.createElement('ul');
+    li.textContent = '緯度：'+data.coord.lon;
+    ul.insertAdjacentElement('beforeend', li);
+    div.insertAdjacentElement('beforeend', ul);
+    li2.textContent = '経度：'+data.coord.lat;
+    ul.insertAdjacentElement('beforeend', li2);
+    li3.textContent = '天気：'+data.weather[0].description;
+    ul.insertAdjacentElement('beforeend', li3);
+    li4.textContent = '最低気温：'+data.main.temp_min;
+    ul.insertAdjacentElement('beforeend', li4);
+    li5.textContent = '湿度：'+data.main.humidity;
+    ul.insertAdjacentElement('beforeend', li5);
+    li6.textContent = '風速：'+data.wind.speed;
+    ul.insertAdjacentElement('beforeend', li6);
+    li7.textContent = '風向：'+data.wind.deg;
+    ul.insertAdjacentElement('beforeend', li7);
+    li8.textContent = '都市名：'+data.name ;
+    ul.insertAdjacentElement('beforeend', li8);    
+  
+    }
+    // 通信エラーが発生した時の処理
+    function showError(err) {
+      console.log(err);
+    }
+
+    // 通信の最後にいつも実行する処理
+    function finish() {
+      console.log('Ajax 通信が終わりました');
+    }
 
